709970fa46856482490c3a57dfbc4736271ef627
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-22 22:22:25 -0300

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import './style.scss'
-
-console.log("hi there")
+import './styles/main.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,16 @@ module.exports = {
       },
       {
         test: /\.scss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
     ]
   },
   plugins: [
