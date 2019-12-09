9968a3e449da9b414eac608eb647f0c801fe57b9
Manuel Garcia F <mgarciafrigo@gmail.com>
2019-12-09 19:36:25 -0300

    add comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  // Set development mode for a more readable bundle.
+  // Remove in production.
   mode: 'development',
+
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  // Loaders.
   module: {
     rules: [
       {
+        // Html-loader, set to minify html.
         test: /\.html$/,
         use: [
           {
@@ -21,21 +27,26 @@ module.exports = {
         ],
       },
       {
+        // Sass-loader.
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
+        // File-loader for media.
         test: /\.(png|svg|jpe?g|gif)$/i,
         use: ['file-loader'],
       },
       {
+        // File-loader for fonts.
         test: /\.(woff2?|eot|ttf|otf)$/,
         use: ['file-loader'],
       },
     ],
   },
   plugins: [
+    // CleanWebpackPlugin cleans the build folder before each build.
     new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
