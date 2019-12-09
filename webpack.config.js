b258f7c3c2a1ada931f7137b87fc7be8ac8e416b
Manuel Garcia F <mgarciafrigo@gmail.com>
2019-12-09 19:15:06 -0300

    add eslint and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,44 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader"
-          }
-        ]
-      }
-    ]
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html"
-    })
-  ]
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
