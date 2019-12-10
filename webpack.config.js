a1230002b979f796d2cf783865b05652130d4c00
Manuel Garcia F <mgarciafrigo@gmail.com>
2019-12-10 14:05:48 -0300

    add babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,17 @@ module.exports = {
   module: {
     rules: [
       {
+        // Babel-loader
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         // Html-loader, set to minify html.
         test: /\.html$/,
         use: [
